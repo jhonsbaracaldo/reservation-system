b823b89da6e4390dd710aa12d3afffd4770f5230
jhonsbaracaldo <jhonsbaracaldo@gmail.com>
2024-01-24 10:17:43 -0500

    Create project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ReservationComponent,
+    ReservationListComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
